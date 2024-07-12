6326285ee641b42bf7c3079f1be31b49654eef74
Vishal <raivishal2018@gmail.com>
2024-07-12 11:06:40 +0530

    Data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,7 @@ from src.logger import logging
 from src.exception import CustomException
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
-#from src.component.data_transfromation import DataTransformation
+from src.components.data_transformation import DataTransformation
 #from src.component.modrl_trainer import ModelTrainer
 
 @dataclass
@@ -45,14 +45,14 @@ class DataIngestion:
             )
         except Exception as e:
             logging.info("Error occured in data ingestion stage")
-            raise CustmeException(e, sys)
+            raise CustomException(e, sys)
 
 if __name__ =="__main__":
     obj = DataIngestion()
     train_data_path , test_data_path = obj.inititate_data_ingestion()
 
-   # data_transformation = DataTransformation()
-    #train_arr, test_arr, _ = data_transformation.inititate_data_transformation(treain_data_path , test_data_path)
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.inititate_data_transformation(train_data_path , test_data_path)
 
    # modeltrainer = ModelTrainer()
     #print(modeltrainer.inititate_model_trainer(train_arr, test_arr))
